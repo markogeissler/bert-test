06aefe7575ed7f5548f759ca0f50a9eab2ac62be
Marko <marko.geissler@gmail.com>
2024-01-06 14:44:36 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained("younoger/YGBNumbersBert")
+    return tokenizer,model
+
+
+tokenizer,model = get_model()
+
+user_input = st.text_area('Nachricht')
+button = st.button("Auswerten")
+
+d = {
+    
+  1:'Keine Nummer',
+  0:'Nummer'
+}
+
+if user_input and button :
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
